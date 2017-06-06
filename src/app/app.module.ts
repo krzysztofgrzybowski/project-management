c7e6b1cd3f7f58276def805d9421deec989b152d
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-06-06 16:55:02 +0200

    Add athentication dialog box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomepageComponent,
+    AuthDialogComponent
   ],
   imports: [
     BrowserModule,

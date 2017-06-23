79ae3a2d49488d499867ee99ac6117cd99a8f1c5
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-06-23 13:30:00 +0200

    Add working log in form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Angular2TokenService } from 'angular2-token';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     NavComponent,
     HomepageComponent,
     AuthDialogComponent,
-    DashboardComponent
+    DashboardComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +26,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    Angular2TokenService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/log-in/log-in.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.sass']
+})
+export class LogInComponent implements OnInit {
+  private logInUser = {
+    email: '',
+    password: ''
+  };
+  hasErrors: boolean;
+  errorMsgArray: string[];
+
+  constructor(
+    private tokenService: Angular2TokenService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogInSubmit() {
+      this.tokenService.signIn(this.logInUser).subscribe(
+        () => {
+          this.hasErrors = false;
+          this.router.navigate(['/dashboard'])
+        },
+        (response) => {
+          this.hasErrors = true;
+          this.errorMsgArray = response.json()['errors']
+          this.logInUser = {
+            email: '',
+            password: ''
+          };
+        }
+      );
+    }
+
+}

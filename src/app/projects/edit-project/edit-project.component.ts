ae71fa57b74e03b911e34761afbffe134f565942
krzysztofgrzybowski <krzysztof.grzybowski@polcode.net>
2017-06-27 15:58:07 +0200

    Add project edit page

--- src/app/projects/edit-project/edit-project.component.ts
+++ src/app/projects/edit-project/edit-project.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Project, ProjectService } from '../../services/project.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-project',
@@ -7,30 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProjectComponent implements OnInit {
 
-  private editProject: Project;
+  private editProject: Project = {
+    id: 0,
+    name: ''
+  };
+  projectId: number;
   hasErrors: boolean;
   errorMsgArray: string[];
 
   constructor(
     private _projectService: ProjectService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
+    this.projectId = this.route.snapshot.params['id']
   }
 
   ngOnInit() {
+    this.editProject = this._projectService.getProject(this.projectId).subscribe(
+      res => {
+        this.editProject = res;
+      }
+    );
+    console.log(this.editProject);
   }
 
   onUpdateSubmit() {
-    this._projectService.updateProject(this.editProject).subscribe(
-      res => {
-        this.router.navigate(['/projects']);
-      },
-      error => {
-        this.hasErrors = true;
-        this.errorMsgArray = error.json()["errors"];
-        console.log(this.errorMsgArray);
-      }
-    );
+
   }
 
 }
